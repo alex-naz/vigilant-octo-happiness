c8093cd3b20e86f26661b068aeba2ab45a55ca8a
alex-naz <alexnazareth02@gmail.com>
2024-03-12 18:51:27 +0000

    Adding the cython, setup, run scripts for initial cythonization of code.

---
+++ LL_run.py
@@ -0,0 +1,13 @@
+import sys
+from LL_cython import main
+
+if __name__ == '__main__':
+    if int(len(sys.argv)) == 5:
+        PROGNAME = sys.argv[0]
+        ITERATIONS = int(sys.argv[1])
+        SIZE = int(sys.argv[2])
+        TEMPERATURE = float(sys.argv[3])
+        PLOTFLAG = int(sys.argv[4])
+        main(PROGNAME, ITERATIONS, SIZE, TEMPERATURE, PLOTFLAG)
+    else:
+        print("Usage: python {} <ITERATIONS> <SIZE> <TEMPERATURE> <PLOTFLAG>".format(sys.argv[0]))
